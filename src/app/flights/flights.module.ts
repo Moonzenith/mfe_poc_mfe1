167a8d0e7cebad190ad9524a5fabbc1414fc9999
Alejandro Quiroz <alejandroq1113@gmail.com>
2023-01-09 09:24:05 -0500

    Listening for host module configuration via events
    - Dynamic title
    - Pre-filled form

--- src/app/flights/flights.module.ts
+++ src/app/flights/flights.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { FlightsSearchComponent } from './flights-search/flights-search.component';
 import { RouterModule } from '@angular/router';
 import { FLIGHTS_ROUTES } from './flights.routes';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule.forChild(FLIGHTS_ROUTES)
+    RouterModule.forChild(FLIGHTS_ROUTES),
+    FormsModule,
   ],
   declarations: [
     FlightsSearchComponent
